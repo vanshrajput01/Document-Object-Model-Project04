5a06a10e58ff5fd04f763a443ba1337d7ff23b5a
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2023-06-18 11:45:43 +0530

    Project.js

---
+++ Project.js
@@ -0,0 +1,14 @@
+function insert_Row() {
+    var a = document.getElementById('FristTable').insertRow(3);
+    var b = a.insertCell(0);
+    var c = a.insertCell(1);
+    var d = a.insertCell(2);
+    b.innerHTML = "TCS";
+    c.innerHTML = "Meerut";
+    d.innerHTML = "India";
+}
+
+
+
+
+
